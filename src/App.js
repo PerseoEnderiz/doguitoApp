04cd19c3b0bae3fa2aa8f57190156d3d3c84e83c
Perseo Enderiz <perseoheroico@gmail.com>
2023-08-24 01:02:12 -0600

    Proyecto inicial

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+import Page404 from './pages/Page404';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <>
+      <Router>
+        {/* Todo esto permanece en todas las paginas */}
+        <Header/>
+        {/* Rutas generadas por javascript dinamicamente */}
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sobre" element={<Sobre/>} />
+          <Route path="*" element={<Page404/>} />
+
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
